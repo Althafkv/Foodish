4ebaf461d31c3d9d63af2c3c61978a7b58feb464
Althafkv <althafkvworks@gmail.com>
2023-03-20 20:03:56 +0530

    Foodish Tailwind Css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    // fontSize: {
+    //   sm: '0.8rem',
+    //   base: '1rem',
+    //   xl: '1.25rem',
+    //   '2xl': '1.563rem',
+    //   '3xl': '1.900rem',
+    //   '4xl': '2.441rem',
+    //   '5xl': '3.900rem',
+    // },
+    extend: {},
+  },
+  plugins: [],
+}
